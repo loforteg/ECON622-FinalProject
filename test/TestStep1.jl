6f05b8ae02aeca0d7bf5c02fbd46a7fc757dd38b
loforteg <giulialoforte@gmail.com>
2020-12-15 11:10:43 -0800

    Update TestStep1.jl
    check grid search

--- test/TestStep1.jl
+++ test/TestStep1.jl
@@ -5,9 +5,36 @@ include("Step1.jl")
 
 @testset "Step1.jl" begin
 
-    # Test 1: check sizes
+    # Setting
+    β = 0.9932
+    eH = 1.0
+    eL = 0.1
+    E = [eL; eH]
+    # Transition matrix: Pr(j|i) = Π(i,j) (columns sum to 1)
+    πHH = 0.925 # e_H | e_H
+    πHL = 0.5   # e_H | e_L
+    Π = [1 - πHL  πHL
+        1 - πHH πHH]
+    q0 = 1.3
+    σ = 1.5
+    a_lb = -2.0
+    a_max = 5
+    a_size = 100
+    A = range(a_lb, a_max, length = a_size)
+    A = [A; ];
 
+    # Test 1: check β makes sense
+    β = 1.1
+    @test step1(A, E, Π, q, β, σ) === nothing
 
+    # Test 2: check transition matrix
+    β = 0.9932
+    Π = [0.3 0.5
+        0.5 0.5]
+    @test step1(A, E, Π, q, β, σ) === nothing
 
+    # Test 3: check that the price makes sense
+    q = 0.8
+    @test step1(A, E, Π, q, β, σ) === nothing
 
 end
