0372133a340de3d80610268d114642a9ccf77e52
loforteg <giulialoforte@gmail.com>
2020-12-15 18:07:13 -0800

    Update TestStep1EndGrid.jl

--- test/TestStep1EndGrid.jl
+++ test/TestStep1EndGrid.jl
@@ -1,7 +1,7 @@
 # Perform some tests on Step1EndGrid
 
 using Test
-include("Step1EndGrid.jl")
+include("../src/Step1EndGrid.jl")
 
 @testset "Step1EndGrid.jl" begin
 
@@ -15,7 +15,7 @@ include("Step1EndGrid.jl")
     πHL = 0.5   # e_H | e_L
     Π = [1 - πHL  πHL
         1 - πHH πHH]
-    q0 = 1.3
+    q = 1.3
     σ = 1.5
     a_lb = -2.0
     a_max = 5
@@ -26,20 +26,20 @@ include("Step1EndGrid.jl")
 
     # Test 1: check β makes sense
     β = 1.1
-    @test EndGridSearch(A, E, Π, β, σ, q) === nothing
+    @test_throws AssertionError EndGridSearch(A, E, Π, β, σ, q)
 
     # Test 2: check transition matrix
     β = 0.9932
     Π = [0.3 0.5
         0.5 0.5]
-    @test EndGridSearch(A, E, Π, β, σ, q) === nothing
+    @test_throws AssertionError EndGridSearch(A, E, Π, β, σ, q)
 
     # Test 3: check that the price makes sense
     q = 0.8
-    @test EndGridSearch(A, E, Π, β, σ, q) === nothing
+    @test_throws AssertionError EndGridSearch(A, E, Π, β, σ, q)
 
     # Test 4: check that σ is appropriate
     σ = 0.9
-    @test EndGridSearch(A, E, Π, β, σ, q) === nothing
+    @test_throws AssertionError EndGridSearch(A, E, Π, β, σ, q)
 
 end
