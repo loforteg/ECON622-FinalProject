34dce0fd93678aa88ac70d223886cb2824373b36
loforteg <giulialoforte@gmail.com>
2020-12-16 16:46:39 -0800

    Update Step1EndGrid.jl
    RHSEuler and initialpolicy now work. Still problems in LinearInterpolation for EndGridSearch

--- src/Step1EndGrid.jl
+++ src/Step1EndGrid.jl
@@ -14,7 +14,7 @@ using BenchmarkTools, Interpolations, Roots
     Takes price q as given.
 """
 
-function RHSEuler(A, E, Π, q, β, σ, a, aprime; tol = 0.01)
+function RHSEuler(A, E, Π, q, β, σ, e, avalue, aprime; tol = 0.01)
 
     # a is the actual value of assets
     # aprime is the actual of the new assets
@@ -28,7 +28,8 @@ function RHSEuler(A, E, Π, q, β, σ, a, aprime; tol = 0.01)
         - q: price (guessed one)
         - β: discount factor
         - σ: preference parameter
-        - a: actual value of assets today
+        - e: position parameter for the shock (position, not value)
+        - a: actual value of assets today (value, not position)
         - aprime: actual value of assets tomorrow
         - Aprimetol: provisional grid of future assets
     Output:
@@ -47,20 +48,17 @@ function RHSEuler(A, E, Π, q, β, σ, a, aprime; tol = 0.01)
     # (in the future, make it more general)
     lowerbound = 1E-7
     up(c::Real) = (max(c, lowerbound)).^(-σ)
-    #up(a, e, ap, q) = ((a + e - ap*q) <= 0) ? -Inf : (a + e - ap*q).^(-σ)
 
     Uprime = zeros(e_size, 1)
     RHS = zeros(e_size, 1)
 
-    for e = 1:e_size
+    aux = LinearInterpolation(Agrid[:, e], Aprimetol[:, e])
+    adoubleprime = aux(aprime)
+    Uprime = up(aprime .+ E[e] .- q .* adoubleprime)
+    temp = Uprime .* ones(e_size, 1)
+    temp2 = (Π[e, :]' * temp)[1,1]
+    RHS = up(avalue + E[e] - aprime * q) - β * temp2
 
-        aux = LinearInterpolation(Agrid[:, e], Aprimetol[:, e])
-        adoubleprime = aux(aprime)
-        #adoubleprime = LinearInterpolation(Agrid[:, e], Aprimetol[:, e])(aprime)
-        Uprime[e] = up(aprime .+ E[e] .- q .* adoubleprime)
-        RHS[e] = up(a + E[e] - aprime * q) - β * dot(Π[e, :], Uprime)
-
-    end
 
     return RHS
 
@@ -100,25 +98,15 @@ function initialpolicy(A, E, Π, β, σ, q; maxT = 600, tol = 0.01)
         for a = 1:a_size
             for e = 1:e_size
 
-                FOCaprime(aprime) = RHSEuler(A, E, Π, q, β, σ, Agrid[a, e], aprime)
+                FOCfunction(aprime) = RHSEuler(A, E, Π, q, β, σ, e, Agrid[a, e], aprime)
 
-                if FOCaprime(Agrid[1, e])[e] > 0.0
+                if FOCfunction(Agrid[1, e]) > 0.0
                     Aprime[a, e] = Agrid[1, e]
-                elseif FOCaprime(Agrid[end, e])[e] < 0.0
+                elseif FOCfunction(Agrid[end, e]) < 0.0
                     Aprime[a, e] = Agrid[end, e]
                 else
-                    if e == 1
-                        auxfunc1(aprime) = RHSEuler(A, E, Π, q, β, σ, Agrid[a, e], aprime)[1]
-                        Aprime[a, e] = fzero(auxfunc1, Agrid[175, e])
-                    else
-                        auxfunc2(aprime) = RHSEuler(A, E, Π, q, β, σ, Agrid[a, e], aprime)[2]
-                        Aprime[a, e] = fzero(auxfunc2, Agrid[1, e], Agrid[end, e])
-                    end
+                    Aprime[a, e] = fzero(FOCfunction, Agrid[1, e], Agrid[end, e])
                 end
-                # 110-116 may not be working! Check how to adjust it.
-                # Maybe use something different from a bisection method.
-                #    Aprime[a, e] = fzero(FOCaprime, Agrid[1, e], Agrid[end, e])
-                # end
 
             end
         end
@@ -149,22 +137,25 @@ function EndGridSearch(A, E, Π, β, σ, q; maxT = 600, tol = 0.01)
         - pol_func: optimal policy function
     """
 
-    # Define the inverse of the first derivative of utility function
-    # (in the future, make it more general)
-    upinv(B) = B.^(-1/σ)
-
     # Check that parameters and price make sense
     @assert β < 1
     @assert σ > 1
     @assert q > β
 
-    a_size = length(A)
-    e_size = size(E)[1]
-
     # Check that transition matrix is well defined
     for e = 1:e_size
         @assert sum(Π[e,:]) == 1
     end
+
+    a_size = length(A)
+    e_size = size(E)[1]
+
+    # Define the first derivative of utility function and its inverse
+    # (in the future, make it more general)
+    lb = 1E-7
+    lowerbound = lb .* ones(a_size, e_size)
+    up(c) = max.(c, lowerbound).^(-σ)
+    upinv(B) = B.^(-1/σ)
 
     # Compute Aprime: initial grid of next period's assets
     Aprime = initialpolicy(A, E, Π, β, σ, q)
@@ -188,8 +179,8 @@ function EndGridSearch(A, E, Π, β, σ, q; maxT = 600, tol = 0.01)
         A = Ctilda + Aprime .* q .- E'
 
         # Adjust for whether budget constraint binds (only low shock)
-        binds = Aprime .< A[:,1]'
-        Cupdate[binds] .= (Aprime .+ E' .- Aprime[:,1]' .* q')[binds]
+        binds = Aprime .< A[:,1]
+        Cupdate[binds] .= (Aprime .+ E' .- Aprime[:,1] .* q)[binds]
         # Generate auxiliary matrix for when binding
         auxbinding = similar(A)
         for e = 1:e_size
